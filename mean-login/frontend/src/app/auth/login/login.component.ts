2b34bfe1b84654fda138d714c4e206c11a9aea0d
Your Name <jhanrcc@hotmail.com>
2021-10-18 01:32:41 -0500

    edit .css login and register

--- mean-login/frontend/src/app/auth/login/login.component.ts
+++ mean-login/frontend/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { UserI } from '../../models/user';
+ 
 
 
 @Component({
